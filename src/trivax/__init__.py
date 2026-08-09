79c4cc2cd0bf0a32428ed51a537bc43d7cade786
Marcelo Roldão Matos <marceloroldao@gmail.com>
2026-08-09 16:04:44 -0300

    Export baselines and experimental controller

--- src/trivax/__init__.py
+++ src/trivax/__init__.py
@@ -1,9 +1,14 @@
 """TRIVAX experimental adaptive edge control runtime."""
 
+from .baselines import PerturbAndObserve
 from .core import ResolutiveState, TrivaxController
+from .experimental import AdaptiveState, CoherenceAdaptiveController
 from .simulation import ScalarPlant, run_closed_loop
 
 __all__ = [
+    "AdaptiveState",
+    "CoherenceAdaptiveController",
+    "PerturbAndObserve",
     "ResolutiveState",
     "TrivaxController",
     "ScalarPlant",
